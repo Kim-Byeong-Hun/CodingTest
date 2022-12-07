328a60b40cf4a55113be630fbd7b65b4527311fc
qudgns5129 <byeonghuns98@gmail.com>
2022-12-07 17:24:12 +0900

    step4 completion
    step4 completion

--- BaekJoon/step4.py
+++ BaekJoon/step4.py
@@ -56,6 +56,34 @@ print(sum(c) / len(c))
 # 8958번
 import sys
 a = int(input())
+b = []
 for i in range(a):
-    b = sys.stdin.readline().rstrip()
-    
+    b += [sys.stdin.readline().rstrip()]
+
+for q in b:
+    c = 0
+    d = q.split('X')
+    for i in d:
+        for j in range(1, len(i)+1):
+            c += j
+    print(c)
+
+# 4344번
+import sys
+a = int(input())
+b = []
+for i in range(a):
+    b += [list(map(int, sys.stdin.readline().rstrip().split()))]
+
+for i in b:
+    n = i[0]
+    c = 0
+    d = 0
+    for j in range(1, n+1):
+        c += i[j]
+    average = c/n
+    for j in range(1, n+1):
+        if average < i[j]:
+            d += 1
+    e = round(d/n * 100, 3)
+    print('{:.3f}%'.format(e))
